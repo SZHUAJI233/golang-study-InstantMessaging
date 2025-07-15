b9fad68150bd4f5a3008e646f9f8c8eb69eeaa8b
SZHUAJI233 <368971921@qq.com>
2025-07-15 20:57:50 +0800

    v0.3 用户下线以及消息广播

--- v0.2 用户上线及广播/server.go
+++ v0.2 用户上线及广播/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"sync"
 )
@@ -62,29 +61,6 @@ func (server *Server) Handler(conn net.Conn) {
 
 	// 广播当前用户上线消息
 	server.BroadCast(user, "已上线")
-
-	// 接受客户端发送的消息
-	go func() {
-		buf := make([]byte, 4096)
-		for {
-			n, err := conn.Read(buf)
-			if n == 0 {
-				server.BroadCast(user, "下线")
-				return
-			}
-			if err != nil && err != io.EOF {
-				fmt.Println("conn Read err:", err)
-				return
-			}
-
-			// 提取用户消息，去除\n
-			msg := string(buf[:n-1])
-
-			// 将得到的消息进行广播
-			server.BroadCast(user, msg)
-
-		}
-	}()
 
 	// 当前handler阻塞
 	select {}
